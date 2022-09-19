161ff434b83d5cd1638bce8ca93e14ad72b499a1
saparnipurwanto <antoir050996@gmail.com>
2022-09-19 15:31:38 +0700

    menambahkan CI

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama Saya Saparni Purwanto
+            Saya tinggal di daerah Pontianak
+            Olahraga yang saya sukai adalah
+            Voli";
+    }
+}
